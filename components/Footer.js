bcecb90099e3b27e2e2dbd57ff05575faac85101
Adam Preston <adam.m.preston@outlook.com>
2023-07-06 16:50:25 -0600

    just the logo

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/cropped-MERLA-s2sproposal-v3-32x32.png" alt="Netlify Logo" className={styles.logo} />
+        
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>MERLA.dev</title>
+        <title>merla.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="merla.dev" />
+      <img src="/RTI-MERLA_fullCOLOR-5218-flat.png" alt="RTI"  />
+        
       </main>
 
       <Footer />
